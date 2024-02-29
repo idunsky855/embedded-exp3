558f8492f130c69b6503054f70f95d69333d2af0
Idan <105287693+idunsky855@users.noreply.github.com>
2024-02-29 23:18:03 +0200

    initialized project

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <xc.h>
+#include <./pic32m-libs/string.h>
+#include <./pic32m-libs/stdio.h>
+
+#pragma config JTAGEN = OFF 
+#pragma config FWDTEN = OFF
+#pragma config FNOSC = FRCPLL
+#pragma config FSOSCEN = OFF
+#pragma config POSCMOD = EC
+#pragma config OSCIOFNC = ON
+#pragma config FPBDIV = DIV_1
+#pragma config FPLLIDIV = DIV_2
+#pragma config FPLLMUL = MUL_20
+#pragma config FPLLODIV = DIV_1
+
+
+void main() {
+    
+}
